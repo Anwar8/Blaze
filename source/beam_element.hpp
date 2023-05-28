a927963e68942b73151bc0e6ade94f9b54f4d20c
s2259894 <s2259894@sms.ed.ac.uk>
2023-05-28 17:43:14 +0800

    created a copy constructor and assignment by using Sage, and created working tests and test fixtures for rigid body motion

--- source/beam_element.hpp
+++ source/beam_element.hpp
@@ -63,6 +63,32 @@ class beam_element {
 
         void set_d(vec new_disp) {local_d = new_disp;}
 
+    beam_element(const beam_element& other) {
+        id = other.id;
+        nodes = other.nodes;
+        section = other.section;
+        shape_func = other.shape_func;
+        length = other.length;
+        local_d = other.local_d;
+        local_f = other.local_f;
+        local_eps = other.local_eps;
+    }
+    // Copy assignment - i need to learn a bit more...
+    beam_element& operator=(const beam_element& other) {
+        if (this != &other) {
+            beam_element temp(other); // use copy constructor to create temporary object
+
+            std::swap(id, temp.id);
+            std::swap(nodes, temp.nodes);
+            std::swap(section, temp.section);
+            std::swap(shape_func, temp.shape_func);
+            std::swap(length, temp.length);
+            std::swap(local_d, temp.local_d);
+            std::swap(local_f, temp.local_f);
+            std::swap(local_eps, temp.local_eps);
+        }
+        return *this;
+    }
 
 };
 
--- source/tests/my_test.cpp
+++ source/tests/my_test.cpp
@@ -4,27 +4,72 @@
 #include "../maths_defaults.hpp"
 #include "../node.hpp"
 
+#define TOLERANCE 1e-12
 
-TEST(RigidBodyMotion, TestFunction2) {
-    
-    std::array<node, 2> in_nodes;
-    in_nodes[0] = node(0.0, 0.0, 0.0);
-    in_nodes[1] = node(3.0, 0.0, 0.0);
+class RigidBodyMotionTest : public ::testing::Test {
+    // Declare variables to be used in the fixture
+public:
+    std::array<node, 2> in_nodes = {node(0.0, 0.0, 0.0), node(3.0, 0.0, 0.0)};
+    beam_element my_beam;
+    vec d;
+    void SetUp() override {
+        // Create the nodes
+        my_beam = beam_element(in_nodes);
+        my_beam.calc_N(1.5);
+        my_beam.calc_B(1.5);
 
-    beam_element my_beam(in_nodes);
-    my_beam.calc_N(1.5);
-    my_beam.calc_B(1.5);
-    vec d = make_xd_vec(6);
-    d.setZero();
-
-    d(0) = 1; 
-    d(3) = 1;
-    my_beam.set_d(d);
-    my_beam.calc_eps();
-    vec eps = my_beam.get_eps();
-    EXPECT_FLOAT_EQ(eps(0), 0.0);
-    EXPECT_FLOAT_EQ(eps(1), 0.0);
+        // Create the d vector
+        d = make_xd_vec(6);
+    }
+    void TearDown() override {
+        // Reset the variables to their initial state    
+        d.setZero();
 }
+
+};
+
+TEST_F(RigidBodyMotionTest, MoveRight) {
+  // Modify the d vector for this test case
+  d(0) = 1;
+  d(3) = 1;
+  my_beam.set_d(d);
+
+  // Calculate eps and perform assertions
+  my_beam.calc_eps();
+  vec eps = my_beam.get_eps();
+  EXPECT_NEAR(eps(0), 0.0, TOLERANCE);
+  EXPECT_NEAR(eps(1), 0.0, TOLERANCE);
+}
+
+TEST_F(RigidBodyMotionTest, MoveUp) {
+  // Modify the d vector for this test case
+  d(1) = 1;
+  d(4) = 1;
+  my_beam.set_d(d);
+
+  // Calculate eps and perform assertions
+  my_beam.calc_eps();
+  vec eps = my_beam.get_eps();
+  EXPECT_NEAR(eps(0), 0.0, TOLERANCE);
+  EXPECT_NEAR(eps(1), 0.0, TOLERANCE);
+}
+
+TEST_F(RigidBodyMotionTest, RotateCCW) {
+  // Modify the d vector for this test case
+  d(1) = -1; 
+  d(2) = 2.0/3;
+  d(4) = 1;
+  d(5) = 2.0/3;
+  my_beam.set_d(d);
+
+  // Calculate eps and perform assertions
+  my_beam.calc_eps();
+  vec eps = my_beam.get_eps();
+  EXPECT_NEAR(eps(0), 0.0, TOLERANCE);
+  EXPECT_NEAR(eps(1), 0.0, TOLERANCE);
+  
+}
+
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
