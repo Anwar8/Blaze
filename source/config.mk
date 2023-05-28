907434abe5e015d8e8848006dfb23fe71b58a828
s2259894 <s2259894@sms.ed.ac.uk>
2023-05-28 17:06:52 +0800

    Updated Makefile to work with tests. Got first working test.

--- source/config.mk
+++ source/config.mk
@@ -1,7 +1,7 @@
 TARGET= xblaze
 SRCS= maths_defaults.cpp node.cpp beam_element.cpp main.cpp
 
-TEST_TARGET=xblaze_tests
+TEST_TARGET=test_xblaze
 TEST_DIR=tests
 TEST_SRCS= my_test.cpp
 
@@ -17,6 +17,7 @@ EIGEN_PATH= /opt/homebrew/Cellar/eigen/3.4.0_1/include/eigen3
 GTEST_DIR = /opt/homebrew/Cellar/googletest/1.13.0
 # compiler and settings
 CXX= g++-12
+TEST_CXX= g++
 CXXFLAGS= -std=c++20
 else
 # include paths
@@ -24,13 +25,12 @@ EIGEN_PATH= /opt/homebrew/Cellar/eigen/3.4.0_1/include/eigen3
 GTEST_DIR= 
 # compiler and settings
 CXX= g++
+TEST_CXX= g++
 CXXFLAGS= -std=c++2a
 endif
 
 INCLUDE_FLAGS= -I$(EIGEN_PATH)
 # no linker libraries are needed
 LDLIBS= 
-TEST_INCLUDE_FLAGS= -I$(GTEST_DIR)/include -I$(EIGEN_PATH)
-TEST_LDLIBS= -L$(GTEST_DIR)/lib -lgtest -lgtest_main -lpthread
-
-
+TEST_INCLUDE_FLAGS= -I$(GTEST_DIR)/include -I$(EIGEN_PATH) -pthread
+TEST_LDLIBS= -lgtest -lgtest_main -L$(GTEST_DIR)/lib 
--- source/tests/my_test.cpp
+++ source/tests/my_test.cpp
@@ -1,9 +1,11 @@
+#include <iostream>
 #include "gtest/gtest.h"
 #include "../beam_element.hpp"
 #include "../maths_defaults.hpp"
 #include "../node.hpp"
 
-TEST(MyCodeTest, TestFunction2) {
+
+TEST(RigidBodyMotion, TestFunction2) {
     
     std::array<node, 2> in_nodes;
     in_nodes[0] = node(0.0, 0.0, 0.0);
@@ -14,12 +16,12 @@ TEST(MyCodeTest, TestFunction2) {
     my_beam.calc_B(1.5);
     vec d = make_xd_vec(6);
     d.setZero();
-    d(2) = 1; 
-    d(5) = -1; 
+
+    d(0) = 1; 
+    d(3) = 1;
     my_beam.set_d(d);
     my_beam.calc_eps();
     vec eps = my_beam.get_eps();
-    
     EXPECT_FLOAT_EQ(eps(0), 0.0);
     EXPECT_FLOAT_EQ(eps(1), 0.0);
 }
