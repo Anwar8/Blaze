ca20ad2d6f2afae156f7c36af62a230b7802bfc6
s2259894 <s2259894@sms.ed.ac.uk>
2024-08-02 21:22:35 +1000

    updated material models - I think they should now work

---
+++ source/ElasticPlasticMaterial.hpp
@@ -0,0 +1,162 @@
+/**
+ * @file ElasticPlasticMaterial.hpp
+ * @brief class definition that implements an Elastic-Plastic material.
+ */
+
+#ifndef ELASTIC_PLASTIC_MATERIAL_HPP
+#define ELASTIC_PLASTIC_MATERIAL_HPP
+#include "maths_defaults.hpp"
+#include "Material1D.hpp"
+#include <cmath>
+
+/**
+ * @brief A class for elastic-plastic material with hardening.
+ */
+class ElasticPlasticMaterial : public Material1D {
+protected:
+    real beta = 0.0; /**< this is the fraction of strain increment \f$ \Delta \epsilon \f$ that takes the stress to the \f$\bf{current}\f$ yield stress.*/
+    bool loading = true; /**< is the material currently being continuously loaded by the strain increment? if material is unloading or reloading then this is false.*/
+
+public:
+    ElasticPlasticMaterial(real E, real f, real H)
+    {
+        initialise_material(E, f, H);
+    }
+
+    /**
+     * @brief initialise the material and all its properties.
+     * 
+     * @param E Young's modulus.
+     * @param f yield strength.
+     * @param H hardening parameter.
+     */
+    virtual void initialise_material(real E, real f, real H)
+    {
+        reference_E0 = E;
+        reference_fy0 = f;
+        this->H = H;
+        
+        starting_E = reference_E0;
+        starting_E_t = reference_E0;
+        starting_fy = reference_fy0;
+        starting_fy_bar = reference_fy0;
+        starting_stress = 0.0;
+        starting_strain = 0.0;
+        starting_plastic_strain = 0.0;
+        starting_elastic = false;
+    }
+
+    /**
+     * @brief increments the total strain and calculates the yield function, flow, and hardening.
+     * @details this is based on Bhatti's Isotropic hardening algorithm (2006, Pp. 396 - 397)
+     * @param d_eps the increment in total strain.
+     */
+    virtual void increment_strain(real d_eps)
+    {
+        
+        initialise_current_state(); // Copy the starting_ state variables into the current state variables
+        real delta_s = E * d_eps; // calculate elastic stress increment
+        check_if_loading(delta_s); // evaluate loading vs unloading/reloading states
+        real s = stress + delta_s; // estimate stress if entire step is elastic
+        evolve_yield_surface(); // perform the hardening calculation
+        strain += d_eps; // increment the strain by the strain increment
+
+
+        if (elastic) // if the material starts off in its iteration as elastic
+        {
+            // we check if it will remain elastic in this step, first
+            eval_yield_function(s); // updates the 'elastic' variable based on incremented stress
+            
+            if (elastic) // this means the material did not yield in this load-step
+            {
+                stress = s; // so everything is just elastic - just update the stress and exit this function.
+                return;
+            } else { // the material did, in fact, yield during this step.
+                E_t = E * H/ (E + H); // update the tangent modulus as per Bhatti's similar-triangles approach
+                beta = (fy_bar - abs(stress))/(abs(s) - abs(stress)); // calculate the value of \f$\beta\f$ which is non-zero
+            }
+        } else {
+            // we only enter here if we are plastic to begin with
+            if (loading) // if material is plastic and we are loading, then we continue being plastic - not very interesting.
+            {
+                beta = 0;
+            } else { // otherwise, material is unloading and is thus elastic again
+                elastic = true;
+                E_t = E;
+                stress = s;
+                return;
+            }
+        }
+        stress = stress + beta*delta_s + ((E * H)/(E + H)) * (1 - beta) * d_eps;
+        calc_plastic_flow(d_eps); // increment the plastic strain
+    }
+
+    virtual void check_if_loading(real delta_s)
+    {
+        loading = stress*delta_s >= 0;
+    }
+
+    /**
+     * @brief evaluates the yield function which sets the \ref elastic to True if the material yielded.
+     * @details \f$ \abs{s} < \bar{\sigma_y}\f$.
+     * @param s is the stress state that is being evaluated for yield.
+     */
+    virtual void eval_yield_function(real s)
+    {
+        elastic = abs(s) < fy_bar;
+    }
+
+
+    /**
+     * @brief sets the current state variables to equal the starting starting (previous) state.
+     * 
+     */
+    virtual void initialise_current_state()
+    {
+        E = starting_E;
+        E_t = starting_E_t;
+        fy = starting_fy;
+        fy_bar = starting_fy_bar;
+        stress = starting_stress;
+        strain = starting_strain;
+        plastic_strain = starting_plastic_strain;
+        elastic = starting_elastic;
+    }
+
+    /**
+     * @brief calculates plastic strain rate.
+     * 
+     */
+    virtual void calc_plastic_flow(real d_eps)
+    {
+        plastic_strain = plastic_strain + ((1 - beta)/(H/E))*abs(d_eps);
+    }
+    
+    /**
+     * @brief calculates the evolution of the yield surface based on the hardening rule and accumulated plastic strain.
+     * 
+     */
+    virtual void evolve_yield_surface()
+    {
+        fy_bar = fy + H*plastic_strain;
+    }
+
+    /**
+     * @brief updates the starting state of the material by making the current state the next starting state.
+     * 
+     */
+    virtual void update_starting_state() = 0;
+
+
+
+
+    /**
+     * @brief evolves the temperature of the material, and re-evaluates the material state.
+     * 
+     * @param dT 
+     */
+    virtual void increment_temperature(real dT) = 0;
+
+};
+
+#endif
---
+++ source/Material1D.hpp
@@ -0,0 +1,110 @@
+/**
+ * @file Material1D.hpp
+ * @brief the base class for 1D material.
+ */
+
+#ifndef MATERIAL1D_HPP
+#define MATERIAL1D_HPP
+#include "maths_defaults.hpp"
+
+/**
+ * @brief A base pure virtual class for 1D material. 
+ */
+class Material1D {
+protected:
+    /**
+     * @name reference material properties
+     * @brief material properties that are initialised and do not change.
+     */
+    //@{
+    real reference_E0; /**< Initial Young's modulus of the material regardless of temperature or plasticity.*/
+    real reference_fy0; /**< Initial yield stress of the material.*/
+    real H; /**< The \f$ H \f$ hardening parameter for simple kinematic or isotropic hardening.*/ 
+    
+    //@}
+    /**
+     * @name state variables
+     * @brief material parameters that are updated during state update.
+     * @details to avoid having fictitious plastic strain accumulation, we always start with "previous" state - since this
+     * is always the state we start from, Blaze calls it the "starting" state. All state members not prefixed are current
+     * state variables.
+     */
+    //@{
+    real E;  /**< Current Young's modulus of the material.*/ 
+    real E_t; /**< Current tangent Young's modulus of the material.*/ 
+    real fy; /**< Current yield strength.*/
+    real fy_bar; /**< Current yield stress of the material after hardening/softening - \f$\bar{\sigma}_y\f$ from Bhatti.*/
+    real stress; /**< Current stress in the material.*/
+    real strain; /**< Current total strain in the material.*/
+    real plastic_strain; /**< Current accumulated plastic strain.*/
+    bool elastic; /**< Whether the material is currently still elastic or not.*/
+
+    real starting_E; /**< Young's modulus of the material at the start of the load step.*/ 
+    real starting_E_t; /**< Tangent Young's modulus of the material at the start of the load step.*/ 
+    real starting_fy; /**< yield strength at the start of the load step.*/
+    real starting_fy_bar; /**< Yield stress of the material after hardening/softening at the start of the load step.*/
+    real starting_stress; /**< Stress in the material at the start of the load step.*/
+    real starting_strain; /**< Total strain in the material at the start of the load step.*/
+    real starting_plastic_strain; /**< Accumulated plastic strain at the start of the load step.*/
+    bool starting_elastic; /**< Whether the material is still elastic or not at the start of the load step.*/
+    //@}
+    /**
+     * @name thermo-mechanical variables
+     * @brief currently no implemented.
+     */
+    real temperature; /**< Current temperature of the material. In degrees Celsius. Default is \f$ 20 ^\circ C\f$.*/
+    real thermal_strain; /**< the strain due to thermal expansion \f$\varepsilon_{th} = \alpha \Delta T\f$.*/
+    real alpha; /**< Thermal expansion coefficient.*/
+
+public:
+    /**
+     * @brief initialise the material and all its properties.
+     * 
+     * @param E Young's modulus.
+     * @param f yield strength.
+     * @param H hardening parameter.
+     */
+    virtual void initialise_material(real E, real f, real H) = 0;
+
+    /**
+     * @brief increments the total strain and calculates the yield function, flow, and hardening.
+     * 
+     * @param d_eps the increment in total strain.
+     */
+    virtual void increment_strain(real d_eps) = 0;
+
+    /**
+     * @brief evaluates the yield function which sets the \ref elastic to True if \f$f(\sigma) < 0\f$ and false otherwise.
+     * @param s is the stress state that is being evaluated for yield.
+     */
+    virtual void eval_yield_function(real s) = 0;
+
+    /**
+     * @brief calculates plastic strain rate.
+     * @param d_eps the increment in strain.
+     */
+    virtual void calc_plastic_flow(real d_eps) = 0;
+    
+    /**
+     * @brief calculates the evolution of the yield surface based on the hardening rule and accumulated plastic strain.
+     * 
+     */
+    virtual void evolve_yield_surface() = 0;
+
+    /**
+     * @brief updates the starting state of the material by making the current state the next starting state.
+     * 
+     */
+    virtual void update_starting_state() = 0;
+
+
+    /**
+     * @brief evolves the temperature of the material, and re-evaluates the material state.
+     * 
+     * @param dT 
+     */
+    virtual void increment_temperature(real dT) = 0;
+
+};
+
+#endif // MATERIAL1D_HPP
